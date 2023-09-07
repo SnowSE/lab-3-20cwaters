d4461f4b2fe6026664e5856e18d2406bdd88dec6
Casey Waters <casey.waters@students.snow.edu>
2023-09-07 13:56:54 -0600

    Lab 3

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿//Letter pyramid using 5 lines
+Console.WriteLine("    A    ");
+Console.WriteLine("   BCD   ");
+Console.WriteLine("  EFGHI  ");
+Console.WriteLine(" JKLMNOP ");
+Console.WriteLine("QRSTUVWXYZ");
+
+//Letter pyramid using 1 line
+Console.WriteLine("    A \n   BCD   \n  EFGHI  \n JKLMNOP \nQRSTUVWXYZ");
+
+
+Console.Write("Enter first integer: ");
+int first = int.Parse(Console.ReadLine());
+
+Console.Write("Enter second integer: ");
+int second = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Sum is............{first + second}");
+Console.WriteLine($"Difference is.....{first - second}");
+Console.WriteLine($"Product is........{first * second}");
+Console.WriteLine($"Quotient is.......{first / second}");
+
+
